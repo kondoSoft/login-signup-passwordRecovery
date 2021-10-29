f552779146f1d58f651a6a85f892d1cc25db8c7a
ericeladio <eric.chavarria1409@gmail.com>
2021-10-29 15:05:14 -0500

    add icon to tab nav

--- loginSignupPasswordRecovery/App.js
+++ loginSignupPasswordRecovery/App.js
@@ -4,39 +4,49 @@ import {
   Text,
   View,
 } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Login from './src/views/Login/Login';
 import Home from './src/views/home/Home';
-import ForGotPassword from './src/views/ForGotPassword/ForGotPassword'
+import Section1 from './src/views/section/Section1'
+import Section2 from './src/views/section/Section2'
+import Section3 from './src/views/section/Section3'
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
+
+const screenOptions = (route, color) => {
+  let iconName;
+
+  switch (route.name) {
+    case 'Home':
+      iconName = 'home'
+      break
+    case 'section1':
+      iconName = 'book'
+      break
+    case 'section2':
+      iconName = 'person'
+      break
+    default:
+      iconName = 'add'
+      break
+  }
+  return <Icon name={iconName} color={color} size={24} />;
+}
 
 const App = () => {
 
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Login} />
-        <Tab.Screen name="Settings" component={Home} />
+      <Tab.Navigator 
+        screenOptions={({route}) => ({
+          tabBarIcon: ({color}) => screenOptions(route, color),
+        })}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="section1" component={Section1} />
+        <Tab.Screen name="section2" component={Section2} />
+        <Tab.Screen name="section3" component={Section3} />
       </Tab.Navigator>
     </NavigationContainer>
 
