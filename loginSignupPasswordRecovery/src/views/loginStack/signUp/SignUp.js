4a62e9f8ce6e7822b3c62d58ad292231cdcc08da
ericeladio <eric.chavarria1409@gmail.com>
2021-11-01 17:24:17 -0600

    KeyboardAwareScrollView

--- loginSignupPasswordRecovery/src/views/loginStack/signUp/SignUp.js
+++ loginSignupPasswordRecovery/src/views/loginStack/signUp/SignUp.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { Input, Text } from 'react-native-elements'
 import { Button, View, StyleSheet } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
 
 import Wrapper from "../../../common/Wrapper"
+
+const isIOS = Platform.OS === 'ios'
 
 const SignUp = () => {
     const Schema = Yup.object().shape({
@@ -25,58 +28,68 @@ const SignUp = () => {
 
     return (
         <Wrapper>
-            <View style={{
-                flex: 1,
-            }} />
-            <View
-                style={{ flex: 4 }}
+            <KeyboardAwareScrollView
+                enableOnAndroid
+                extraScrollHeight={isIOS ? 130 : -120}
+                contentContainerStyle={{
+                    borderWidth: 0,
+                    borderColor: 'red',
+                    flex:1,
+                  }}
             >
-                <Formik
-                    initialValues={{ user: '', email: '', PhoneNumber: '', password: '', verifyPassword: '' }}
-                    onSubmit={values => console.log(values)}
-                    validationSchema={Schema}
+                <View style={{
+                    flex: 1,
+                }} />
+                <View
+                    style={{ flex: 4 }}
                 >
-                    {({ handleChange, handleSubmit, values, errors }) => (
-                        <>
-                            <Input
-                                label='User name'
-                                onChangeText={handleChange('user')}
-                                value={values.user}
-                            />
-                            <Text style={styles.textError}>{errors.user}</Text>
-                            <Input
-                                label='Email'
-                                onChangeText={handleChange('email')}
-                                value={values.email}
-                            />
-                            <Text style={styles.textError}>{errors.email}</Text>
-                            <Input
-                                label='Phone number'
-                                onChangeText={handleChange('PhoneNumber')}
-                                value={values.PhoneNumber}
-                            />
-                            <Text style={styles.textError}>{errors.PhoneNumber}</Text>
-                            <Input
-                                label='Password'
-                                onChangeText={handleChange('password')}
-                                value={values.password}
-                            />
-                            <Text style={styles.textError}>{errors.password}</Text>
-                            <Input
-                                label='Verify Password'
-                                onChangeText={handleChange('verifyPassword')}
-                                value={values.verifyPassword}
+                    <Formik
+                        initialValues={{ user: '', email: '', PhoneNumber: '', password: '', verifyPassword: '' }}
+                        onSubmit={values => console.log(values)}
+                        validationSchema={Schema}
+                    >
+                        {({ handleChange, handleSubmit, values, errors }) => (
+                            <>
+                                <Input
+                                    label='User name'
+                                    onChangeText={handleChange('user')}
+                                    value={values.user}
+                                />
+                                <Text style={styles.textError}>{errors.user}</Text>
+                                <Input
+                                    label='Email'
+                                    onChangeText={handleChange('email')}
+                                    value={values.email}
+                                />
+                                <Text style={styles.textError}>{errors.email}</Text>
+                                <Input
+                                    label='Phone number'
+                                    onChangeText={handleChange('PhoneNumber')}
+                                    value={values.PhoneNumber}
+                                />
+                                <Text style={styles.textError}>{errors.PhoneNumber}</Text>
+                                <Input
+                                    label='Password'
+                                    onChangeText={handleChange('password')}
+                                    value={values.password}
+                                />
+                                <Text style={styles.textError}>{errors.password}</Text>
+                                <Input
+                                    label='Verify Password'
+                                    onChangeText={handleChange('verifyPassword')}
+                                    value={values.verifyPassword}
 
-                            />
-                            <Text style={styles.textError}>{errors.verifyPassword}</Text>
-                            <Button
-                                title='Sign up'
-                                onPress={handleSubmit}
-                            />
-                        </>
-                    )}
-                </Formik>
-            </View>
+                                />
+                                <Text style={styles.textError}>{errors.verifyPassword}</Text>
+                                <Button
+                                    title='Sign up'
+                                    onPress={handleSubmit}
+                                />
+                            </>
+                        )}
+                    </Formik>
+                </View>
+            </KeyboardAwareScrollView>
         </Wrapper>
     )
 }
